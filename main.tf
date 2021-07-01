dc6fbbda9db051f50ba0fd7238686a1c6c90ed0b
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-01 12:15:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,69 @@ provider "azurerm" {
   alias = "src"
 }
 
-resource "azurerm_resource_group" "rg" {
-    provider = azurerm.src
-    name = "${var.name}-RG"  
-    location = "UK South"
+locals {
+  servicename = "${var.servicename}-${var.role}-${var.deploy_environment}"
+}
+
+resource "azurerm_eventhub_namespace" "eventhub_namespace" {
+  provider            = azurerm.src
+  name                = "m-${local.servicename}-ns"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  capacity            = 2
+  
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+resource "azurerm_eventhub" "eventhub" {
+  provider            = azurerm.src
+  name                = "m-${local.servicename}-eh"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  resource_group_name = var.resource_group_name
+  partition_count     = 2
+  message_retention   = 7
+}
+
+resource "azurerm_eventhub_consumer_group" "logstash_consumer_group" {
+  provider            = azurerm.src
+  name                = "logstash"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_eventhub_consumer_group" "logging_application_consumer_group" {
+  provider            = azurerm.src
+  name                = "loggingApplication"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_eventhub_authorization_rule" "logstash_rule" {
+  provider            = azurerm.src
+  name                = "logstashAccessKey"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  resource_group_name = var.resource_group_name
+  listen              = true
+  send                = false
+  manage              = false
+}
+
+resource "azurerm_eventhub_authorization_rule" "api_rule" {
+  provider            = azurerm.src
+  name                = "logAccessKey"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  resource_group_name = var.resource_group_name
+  listen              = false
+  send                = true
+  manage              = false
 }
 
