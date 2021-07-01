ef77ee60a9e60e53e43b85b3afba83e181004fc1
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-01 12:16:10 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,18 @@
-variable "name" {
+variable "servicename" {
   type = string
-  default = "test"
 }
-
+variable "deploy_environment" {
+  type = string
+}
+variable "role" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+variable "resource_group_location" {
+  type = string
+}
+variable "sku" {
+  type = string
+}
