7b39cbccd429753b01d20443d0b0d7febbf6cb49
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-01 13:32:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "servicename" {
   type = string
+  description = "the service name"
 }
 variable "deploy_environment" {
   type = string
+  description = "deployment environment"
 }
 variable "role" {
   type = string
+  description = "it's role within the service"
 }
 variable "resource_group_name" {
   type = string
